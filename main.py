1d68e9ca271abd5cd9a4ff7cee37a68f44b96e2a
matth <mdhutchinson@upei.ca>
2022-07-25 13:27:06 -0300

    added setup window to allow variables to be easily changed and adjusted how data is saved.

--- main.py
+++ main.py
@@ -1,218 +1,262 @@
+import csv
 from random import randint
 import pygame
 from math import sin, cos, sqrt, pi
-from csv import writer
 
-# Settings
-randomFlag = True
-setWidths = [20, 60, 30, 50, 40, 30, 20, 50]
-setRadi = [100, 300, 200, 150, 250, 300, 150, 100]
-numberOfTargets = 12
-fullScreen = True
-windowSize = (800,800)
-sensitivity = 10# value greater than 1. Higher the number, the higher the sensitivity
-deadZone = 0.1 #value from 0 to 1
+class game():
 
-# Globals
-targets = []
-pastTarget = 0
-targetWidth = 50
-chosenTarget = 0
-flag = True
-radius = 150
-lastTicks = 0
-currentTicks = 0
-misses = 0
-lastPosition = (0,0)
-data = []
-setCount = 0
+    def __init__(self, fullScreen=False, randomFlag=True, numberOfTargets=12, sensitivity=10, deadZone=0.1, setWidths = [], setRadii = [], windowSize = (800, 800), joystick ="Mouse", user = "Default"):
+        # Settings
+        self.randomFlag = randomFlag
+        self.setWidths = setWidths
+        self.setRadii = setRadii
+        self.numberOfTargets = numberOfTargets
+        self.fullScreen = fullScreen
+        self.windowSize = windowSize
+        self.sensitivity = sensitivity# value greater than 1. Higher the number, the higher the sensitivity
+        self.deadZone = deadZone #value from 0 to 1
+        self.joystick = joystick
+        self.user = user
 
+        self.pastTarget = 0
+        self.targets = []
+        self.chosenTarget = 0
+        self.targetWidth = 50
+        self.flag = True
+        self.radius = 150
+        self.lastTicks = 0
+        self.currentTicks = 0
+        self.misses = 0
+        self.lastPosition = (0,0)
+        self.data = []
+        self.setCount = 0
 
-def draw_ring(surface):
+        #self.play()
 
-    global targets
-    global chosenTarget
-    global pastTarget
-    global numberOfTargets
-    global flag
-    global radius
-    global targetWidth
-    global randomFlag
-    global setCount
-    global setWidths
-    global setRadi
-    x = []
-    y = []
-    # Get angles around ring that targets will be placed at
-    theta = [0]
-    for i in range(0, numberOfTargets-1):
-        theta.append(theta[i]+(360/numberOfTargets))
+    def play_joystick(self):
+        pygame.init()
+        if pygame.joystick.get_count()>0 :
+            stick = pygame.joystick.Joystick(0)
+            for x in range(pygame.joystick.get_count()):
+                if pygame.joystick.Joystick(x).get_name == self.joystick:
+                    stick=pygame.joystick.Joystick(x)
+                    break;
 
-    centerx = pygame.display.get_window_size()[0]/2
-    centery = pygame.display.get_window_size()[1]/2
-
-    # Algo for moving target around ring. Follows the movement shown in paper by I. MacKenzie
-    if pastTarget <= numberOfTargets/2:
-        if flag:
-            if pastTarget <= (numberOfTargets-1)/2 :
-                chosenTarget = pastTarget + int(numberOfTargets/2)
+            pygame.event.pump()
+            pygame.display.init()
+            pygame.event.set_grab(True)
+            if self.fullScreen:
+                window = pygame.display.set_mode((0,0), pygame.FULLSCREEN) # 0, 0, fullscreen
             else:
-                chosenTarget = pastTarget - int(numberOfTargets/2)
-            flag = False
-        else:
+                window = pygame.display.set_mode(self.windowSize)
+            cursor = pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
+            pygame.mouse.set_cursor(cursor)
 
-            chosenTarget = pastTarget
-            pastTarget = pastTarget + 1
-            flag = True
-    else:
-        #reset targets and create new ring
-        flag = True
-        targets = []
+            target = self.draw_ring(window)
 
-        pastTarget = 0
-        chosenTarget = 0
+            going = True
 
-        # If not set to random, use set widths and radi
-        if randomFlag:
-            targetWidth = randint(20, 60)
-            radius = randint(100, 300)
-        else:
-            if setCount >= len(setWidths):
-                setCount = 0
-            targetWidth = setWidths[setCount]
-            radius = setRadi[setCount]
-            setCount = setCount +1
-    # calc x and y position based on angles generated
-    for i in range(len(theta)):
-        x.append(centerx + radius * cos(theta[i] * pi / 180))
-        y.append(centery + radius * sin(theta[i] * pi / 180))
-    # create targets
-    for i in range(len(x)):
-        targets.append(pygame.draw.circle(surface, (50, 50, 50), (x[i], y[i]), targetWidth))
-    # append chosen target at at end so that it is drawn last and on top of all other targets
-    targets.append(pygame.draw.circle(surface, (200, 10, 10), (x[chosenTarget], y[chosenTarget]), targetWidth))
-    pygame.display.flip()
+            while going:
 
-    return targets[chosenTarget]
+                event = pygame.event.poll()
 
-# adds 10px border to window to prevent mouse exiting screen
-def stop_mouse(surface):
-    if pygame.mouse.get_pos()[0] in range(0, 10): #left wall
-        pygame.mouse.set_pos(11, pygame.mouse.get_pos()[1])
-
-    if pygame.mouse.get_pos()[0] in range(surface.get_size()[0]-10, surface.get_size()[0]): # right wall
-        pygame.mouse.set_pos(surface.get_size()[0]-11, pygame.mouse.get_pos()[1])
-
-    if pygame.mouse.get_pos()[1] in range(0, 10): # ceiling
-        pygame.mouse.set_pos(pygame.mouse.get_pos()[0], 11)
-
-    if pygame.mouse.get_pos()[1] in range(surface.get_size()[1]-10, surface.get_size()[1]): # floor
-        pygame.mouse.set_pos(pygame.mouse.get_pos()[0], surface.get_size()[1]-11)
-
-
-# def create_target_random(surface):
-#     radius = random.randint(25, 100)
-#     sizeX = surface.get_size()[0]-radius-10 # -10 added to account for border created by stop_mouse
-#     sizeY = surface.get_size()[1]-radius-10
-#     x = random.randint(radius, sizeX)
-#     y = random.randint(radius, sizeY)
-#
-#     target = pygame.draw.circle(surface, (200, 10, 10), (x, y), radius)
-#     return target
-
-
-# deals with button press's and checks if mouse is within chosen target
-def button_press(currentTarget):
-    global targetWidth
-    global pastTarget
-    global lastTicks
-    global currentTicks
-    global misses
-    global lastPosition
-    dx = currentTarget.centerx - pygame.mouse.get_pos()[0]
-    dy = currentTarget.centery - pygame.mouse.get_pos()[1]
-    dist_sq = dx*dx + dy*dy
-
-    rad = currentTarget.width/2
-
-    if dist_sq < rad*rad:
-
-        window.fill((0, 0 ,0))
-        currentPos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-        currentTarget = draw_ring(window)
-
-        distance = sqrt((lastPosition[0]-currentPos[0])**2+(lastPosition[1]-currentPos[1])**2)
-        lastPosition = currentPos
-
-        if pastTarget > 0:
-            currentTicks = pygame.time.get_ticks()
-            dt = currentTicks - lastTicks
-            lastTicks = currentTicks
-            data.append([dt, misses, targetWidth, distance])
-        else:
-            data.append("###############")
-            lastTicks = pygame.time.get_ticks()
-    else:
-        misses = misses +1
-
-    return currentTarget
-
-# game code
-pygame.init()
-
-if(pygame.joystick.get_count()>0):
-    stick = pygame.joystick.Joystick(0)
-
-    pygame.event.pump()
-    pygame.display.init()
-    pygame.event.set_grab(True)
-    if fullScreen:
-        window = pygame.display.set_mode((0,0), pygame.FULLSCREEN) # 0, 0, fullscreen
-    else:
-        window = pygame.display.set_mode(windowSize)
-    cursor = pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
-    pygame.mouse.set_cursor(cursor)
-
-    target = draw_ring(window)
-
-    going = True
-
-    while going:
-
-        event = pygame.event.poll()
-
-        if event.type == pygame.JOYBUTTONDOWN:
-            if stick.get_button(0):
-                target = button_press(target)
-
-        elif event.type == pygame.JOYAXISMOTION:
-            while stick.get_axis(0) > deadZone or stick.get_axis(0) < -deadZone or stick.get_axis(1) > deadZone or stick.get_axis(1) < -deadZone: #0.1 is the deadzone im applying to deal with jitter
-
-                pygame.time.Clock().tick(90) #Forces code to run at 90fps. Keeps mouse from getting wildly sensative
-
-                pos = pygame.mouse.get_pos()
-                x = float(pos[0])
-                y = float(pos[1])
-                pygame.mouse.set_pos([round(x + stick.get_axis(0)*sensitivity), round(y + stick.get_axis(1)*sensitivity)])#Set_pos uses int's. this is awful. axis values are 0-1 and get rounded down.
-                stop_mouse(window)
-                if pygame.event.peek(pygame.JOYBUTTONDOWN):
+                if event.type == pygame.JOYBUTTONDOWN:
                     if stick.get_button(0):
-                        target = button_press(target)
-                        pygame.event.get()
-                        break
+                        target = self.button_press(target, window)
 
-        if event.type == pygame.QUIT:
-            going = False
+                elif event.type == pygame.JOYAXISMOTION:
+                    while stick.get_axis(0) > self.deadZone or stick.get_axis(0) < -self.deadZone or stick.get_axis(1) > self.deadZone or stick.get_axis(1) < -self.deadZone: #0.1 is the deadzone im applying to deal with jitter
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+                        pygame.time.Clock().tick(90) #Forces code to run at 90fps. Keeps mouse from getting wildly sensative
+
+                        pos = pygame.mouse.get_pos()
+                        x = float(pos[0])
+                        y = float(pos[1])
+                        #print(stick.get_axis(1))
+                        pygame.mouse.set_pos([round(x + stick.get_axis(0)*self.sensitivity), round(y + stick.get_axis(1)*self.sensitivity)])#Set_pos uses int's. this is awful. axis values are 0-1 and get rounded down.
+                        self.stop_mouse(window)
+                        if pygame.event.peek(pygame.JOYBUTTONDOWN):
+                            if stick.get_button(0):
+                                target = self.button_press(target, window)
+                                pygame.event.get()
+                                break
+
+                if event.type == pygame.QUIT:
+                    going = False
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        going = False
+
+            fileName = self.user + ".csv"
+            f = open(fileName, 'a')
+            writer = csv.writer(f)
+            writer.writerow(["sensitivity: "+str(self.sensitivity), "Num of Targets: "+str(self.numberOfTargets), "Deadzone: "+ str(self.deadZone), "Controller: "+self.joystick, "Random: "+str(self.randomFlag)])
+            writer.writerow(["Time","Misses", "Width", "Distance"])
+            writer.writerows(self.data)
+            f.close()
+            pygame.time.wait(10)
+            pygame.quit()
+        else:
+            print("No Joysticks Found")
+
+
+    def play_mouse(self):
+        pygame.init()
+        stick = pygame.mouse
+        pygame.event.pump()
+        pygame.display.init()
+        pygame.event.set_grab(True)
+        if self.fullScreen:
+            window = pygame.display.set_mode((0,0), pygame.FULLSCREEN) # 0, 0, fullscreen
+        else:
+            window = pygame.display.set_mode(self.windowSize)
+        cursor = pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
+        pygame.mouse.set_cursor(cursor)
+
+        target = self.draw_ring(window)
+
+        going = True
+
+        while going:
+
+            event = pygame.event.poll()
+            if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEBUTTONUP:
+                target = self.button_press(target, window)
+
+            elif event.type == pygame.MOUSEMOTION:
+                pygame.time.Clock().tick(90) #Forces code to run at 90fps. Keeps mouse from getting wildly sensative
+                self.stop_mouse(window)
+
+            if event.type == pygame.QUIT:
                 going = False
 
-    f = open('data.csv', 'w')
-    writer = writer(f)
-    writer.writerow(["Time","Misses", "Width", "Distance"])
-    writer.writerows(data)
-    f.close()
-    pygame.time.wait(10)
-else:
-    print("No Joysticks Found")
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    going = False
+        fileName = self.user + ".csv"
+        f = open(fileName, 'a')
+        writer = csv.writer(f)
+        writer.writerow(["sensitivity: "+str(self.sensitivity), "Num of Targets: "+str(self.numberOfTargets), "Deadzone: "+ str(self.deadZone), "Controller: "+self.joystick, "Random: "+str(self.randomFlag)])
+        writer.writerow(["Time","Misses", "Width", "Distance"])
+        writer.writerows(self.data)
+        f.close()
+        pygame.time.wait(10)
+        pygame.quit()
+
+
+    def draw_ring(self, surface):
+
+        x = []
+        y = []
+        # Get angles around ring that targets will be placed at
+        theta = [0]
+        for i in range(0, self.numberOfTargets-1):
+            theta.append(theta[i]+(360/self.numberOfTargets))
+
+        centerx = pygame.display.get_window_size()[0]/2
+        centery = pygame.display.get_window_size()[1]/2
+
+        # Algo for moving target around ring. Follows the movement shown in paper by I. MacKenzie
+        if self.pastTarget <= self.numberOfTargets/2:
+            if self.flag:
+                if self.pastTarget <= (self.numberOfTargets-1)/2 :
+                    self.chosenTarget = self.pastTarget + int(self.numberOfTargets/2)
+                else:
+                    self.chosenTarget = self.pastTarget - int(self.numberOfTargets/2)
+                self.flag = False
+            else:
+
+                self.chosenTarget = self.pastTarget
+                self.pastTarget = self.pastTarget + 1
+                self.flag = True
+        else:
+            #reset targets and create new ring
+            self.flag = True
+            self.targets = []
+
+            self.pastTarget = 0
+            self.chosenTarget = 0
+
+            # If not set to random, use set widths and radi
+            if self.randomFlag:
+                self.targetWidth = randint(20, 60)
+                self.radius = randint(100, 300)
+            else:
+                if self.setCount >= len(self.setWidths):
+                    self.setCount = 0
+                self.targetWidth = self.setWidths[self.setCount]
+                self.radius = self.setRadii[self.setCount]
+                self.setCount = self.setCount +1
+        # calc x and y position based on angles generated
+        for i in range(len(theta)):
+            x.append(centerx + self.radius * cos(theta[i] * pi / 180))
+            y.append(centery + self.radius * sin(theta[i] * pi / 180))
+        # create targets
+        for i in range(len(x)):
+            self.targets.append(pygame.draw.circle(surface, (50, 50, 50), (x[i], y[i]), self.targetWidth))
+        # append chosen target at at end so that it is drawn last and on top of all other targets
+        self.targets.append(pygame.draw.circle(surface, (200, 10, 10), (x[self.chosenTarget], y[self.chosenTarget]), self.targetWidth))
+        pygame.display.flip()
+
+        return self.targets[self.chosenTarget]
+
+    # adds 10px border to window to prevent mouse exiting screen
+    def stop_mouse(self, surface):
+        if pygame.mouse.get_pos()[0] in range(0, 10): #left wall
+            pygame.mouse.set_pos(11, pygame.mouse.get_pos()[1])
+
+        if pygame.mouse.get_pos()[0] in range(surface.get_size()[0]-10, surface.get_size()[0]): # right wall
+            pygame.mouse.set_pos(surface.get_size()[0]-11, pygame.mouse.get_pos()[1])
+
+        if pygame.mouse.get_pos()[1] in range(0, 10): # ceiling
+            pygame.mouse.set_pos(pygame.mouse.get_pos()[0], 11)
+
+        if pygame.mouse.get_pos()[1] in range(surface.get_size()[1]-10, surface.get_size()[1]): # floor
+            pygame.mouse.set_pos(pygame.mouse.get_pos()[0], surface.get_size()[1]-11)
+
+
+
+    # deals with button press's and checks if mouse is within chosen target
+    def button_press(self, currentTarget, window):
+
+        dx = currentTarget.centerx - pygame.mouse.get_pos()[0]
+        dy = currentTarget.centery - pygame.mouse.get_pos()[1]
+        dist_sq = dx*dx + dy*dy
+
+        rad = currentTarget.width/2
+
+        if dist_sq < rad*rad:
+
+            window.fill((0, 0 ,0))
+            currentPos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+            currentTarget = self.draw_ring(window)
+
+            distance = sqrt((self.lastPosition[0]-currentPos[0])**2+(self.lastPosition[1]-currentPos[1])**2)
+            self.lastPosition = currentPos
+
+            if self.pastTarget > 0:
+                self.currentTicks = pygame.time.get_ticks()
+                dt = self.currentTicks - self.lastTicks
+                self.lastTicks = self.currentTicks
+                self.data.append([dt, self.misses, self.targetWidth, distance])
+            else:
+                self.data.append("###############")
+                self.lastTicks = pygame.time.get_ticks()
+        else:
+            self.misses = self.misses +1
+
+        return currentTarget
+
+    def get_joysticks(self):
+        pygame.init()
+        count = pygame.joystick.get_count()
+        sticks = []
+        for x in range(count):
+            sticks.append(pygame.joystick.Joystick(x).get_name())
+        return sticks
+
+
+if __name__ == '__main__':
+    game = game()
+    print("after game")
---
+++ setup.py
@@ -0,0 +1,143 @@
+import tkinter as tk
+from tkinter import filedialog as fk
+import main
+
+root = tk.Tk()
+root.geometry("800x250")
+root.title("Fitts's Law Setup")
+root.resizable(width = False, height = False)
+mainFrame = tk.Frame(root, width=800, height=250, bg = "light blue")
+
+
+
+
+#Bases
+userInfoFrame = tk.Frame(mainFrame, bg = "light blue")
+checkBoxFrame = tk.Frame(mainFrame, bg = "light blue")
+setFrame = tk.Frame(mainFrame, bg = "light blue")
+screenFrame = tk.Frame(mainFrame, bg = "light blue")
+variableFrame = tk.Frame(mainFrame, bg = "light blue")
+startButton = tk.Button(mainFrame, text="Begin", bg = "cyan", command = lambda : begin())
+
+#User info
+userNameLabel = tk.Label(userInfoFrame, text="UserName", bg = "light blue")
+userName = tk.StringVar()
+userName.set("Default")
+userNameBox = tk.Entry(userInfoFrame, textvariable = userName)
+controller = tk.StringVar()
+controller.set("Mouse")
+controllerLabel = tk.Label(userInfoFrame, text="Controller", bg = "light blue")
+sticks = main.game().get_joysticks()
+controllerDrop = tk.OptionMenu(userInfoFrame, controller, "Mouse", sticks)
+
+#Variables
+numTargetsLabel = tk.Label(variableFrame, text="Number of Targets", bg = "light blue")
+numTargetsSpinner = tk.Spinbox(variableFrame, from_=2, to=50)
+sensitivityLabel = tk.Label(variableFrame, text="Sensitivity", bg = "light blue")
+sensitivitySpinner = tk.Spinbox(variableFrame, from_=1, to=50)
+deadZoneLabel = tk.Label(variableFrame, text="Dead Zone", bg = "light blue")
+deadZoneSpinner = tk.Spinbox(variableFrame, from_=1, to=10)
+
+#Check boxes
+randVar = tk.IntVar()
+randVar.set(1)
+randomCheck = tk.Checkbutton(checkBoxFrame,  text = "Random", command = lambda  : setDiag.config(state = "disable") if randVar.get() else setDiag.config(state="active"), variable = randVar, bg = "light blue")
+
+screenVar = tk.IntVar()
+screenCheck = tk.Checkbutton(checkBoxFrame, text = "FullScreen", command = lambda  : disableSize(True) if screenVar.get() else disableSize(False), variable = screenVar, bg = "light blue")
+
+
+def disableSize(off):
+    if(off):
+        screenWEntry.config(state = "disabled")
+        screenHEntry.config(state = "disabled")
+    else:
+        screenWEntry.config(state = "normal")
+        screenHEntry.config(state = "normal")
+
+
+setVar = tk.StringVar()
+setText = tk.Entry(setFrame, textvariable = setVar, state="disable")
+setDiag = tk.Button(setFrame, text = "Choose a csv file", command = lambda : setVar.set(fk.askopenfilename()), state = "disable")
+
+screenLabelW = tk.Label(screenFrame, text = "Width:", bg = "light blue")
+screenLabelH = tk.Label(screenFrame, text = "Height:", bg = "light blue")
+screenW = tk.StringVar()
+screenW.set("800")
+screenH = tk.StringVar()
+screenH.set("800")
+screenWEntry = tk.Entry(screenFrame, textvariable = screenW)
+screenHEntry = tk.Entry(screenFrame, textvariable = screenH)
+
+
+def packWindow():
+    #mainFrame.pack()
+    mainFrame.place(relheight = 1, relwidth = 1)
+    userInfoFrame.place(height = 50, width = 800)
+    variableFrame.place(height = 50, width = 800, y = 60)
+    checkBoxFrame.place(height = 25, width = 800, y = 125)
+
+    setFrame.place(height = 100, width = 400 , y=150)
+    screenFrame.place(height = 100, width = 400, x=400, y=150)
+    startButton.place(height = 50, width = 800, y = 200)
+
+    randomCheck.place(height = 25, width = 400)
+    screenCheck.place(height = 25, width = 400, x = 400)
+
+    userNameLabel.place(height = 25, width = 400)
+    userNameBox.place(height = 25, width = 400, y = 25)
+    controllerLabel.place(height = 25, width = 400, x = 400)
+    controllerDrop.place(height = 25, width = 400, x = 400, y = 25)
+
+    numTargetsLabel.place(height = 25, relwidth=0.34)
+    numTargetsSpinner.place(height = 25, width = 50, relx = 0.15, y =25)
+    sensitivityLabel.place(height = 25, relwidth = 0.34, relx = 0.34)
+    sensitivitySpinner.place(height = 25, width = 50, relx = 0.5, y = 25)
+    deadZoneLabel.place(height = 25, relwidth = 0.34, relx = 0.68)
+    deadZoneSpinner.place(height = 25, width = 50, relx = 0.85, y = 25)
+
+    setDiag.place(height = 25, width = 400, y = 25)
+    setText.place(height = 25, width=400)
+    screenLabelW.place(height = 25, width = 200)
+    screenWEntry.place(height = 25, width = 200, x = 200)
+    screenLabelH.place(height = 25, width = 200, y = 25)
+    screenHEntry.place(height = 25, width = 400, y = 25, x = 200)
+
+
+def begin():
+    name = userName.get()
+    joystick = controller.get()
+    fullScreen = bool(screenVar.get())
+    screenSize = (int(screenW.get()), int(screenH.get()))
+    random = bool(randVar.get())
+    if not random:
+        setWidths, setRadii = readSet()
+    else:
+        setWidths, setRadii = [], []
+    numTargets = int(numTargetsSpinner.get())
+    sensitivity = int(sensitivitySpinner.get())
+    deadZone = int(deadZoneSpinner.get()) / 10
+    newGame = main.game(fullScreen, random, numTargets, sensitivity, deadZone, setWidths, setRadii, screenSize, joystick, name)
+    if joystick == "Mouse":
+        newGame.play_mouse()
+    else:
+        newGame.play_joystick()
+
+
+def readSet():
+    f = open(str(setVar.get()), "r")
+    stringRadii = f.readline()
+    stringWidths = f.readline()
+    f.close()
+    set_radii = [int(x) for x in stringRadii.split(",")]
+    setWidths = [int(x) for x in stringWidths.split(",")]
+
+    return setWidths, set_radii
+
+
+packWindow()
+#main.game().get_joysticks()
+root.mainloop()
+
+
+
